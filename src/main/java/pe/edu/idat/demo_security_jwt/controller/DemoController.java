406b8166f36a8fbb96c1bdec3bcff50e88708529
IdatCicloFinal <Idat6to2025@outlook.com>
2025-03-17 20:26:06 -0500

    CROS origin

--- src/main/java/pe/edu/idat/demo_security_jwt/controller/DemoController.java
+++ src/main/java/pe/edu/idat/demo_security_jwt/controller/DemoController.java
@@ -3,6 +3,7 @@ package pe.edu.idat.demo_security_jwt.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("hasRole('ADMIN')")
 @RestController
 @RequestMapping("api/v1/demo")
+@CrossOrigin(origins = {"http://front-admin.idat.pe",
+        "http://front-docentes.idat.pe"})
 public class DemoController {
 
     @GetMapping("/saludo")
